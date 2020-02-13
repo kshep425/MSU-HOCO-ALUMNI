69a2027e42ed594c78e7a83422ed6280a231e9b0
Keisha <kshep425@gmail.com>
2020-02-12 22:23:44 -0500

    Added passport, changed signup to register

--- tests/login_api_routes.test.js
+++ tests/login_api_routes.test.js
@@ -1,10 +1,11 @@
 const request = require("supertest")
+// const superagent = require("superagent")
 const express = require("express");
 const session = require("express-session");
-//const passport = require("../config/passport");
+const passport = require("../config/passport");
 const initialize_connection = require("../config/db_connection");
 const db_obj = require("../config/db_config")
-const sequelize = initialize_connection(db_obj);
+// const sequelize = initialize_connection(db_obj);
 const {Op} = require("sequelize");
 
 const app = express();
@@ -13,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 require("../routes/login_api_routes")(app)
 let i = 3000;
 
@@ -52,79 +53,79 @@ describe("Login Tests", () => {
     })
 
 
-    it('/api/signup/ allows new user to be created',(done)=>{
-        const member_signup = {
+    it('/api/register/ allows new user to be created',(done)=>{
+        const member_register = {
             username: "test_user_" + i,
             password: "password_" + i,
             full_name: `Jane ${i} Doe`,
             email: `jane${i}@msu.edu`
         }
-        console.log(member_signup)
+        console.log(member_register)
         request(app)
-        .post("/api/signup")
-        .send(member_signup)
+        .post("/api/register")
+        .send(member_register)
         .expect(307, done)
     })
 
-    it('/api/signup/ does not allow duplicate member usernames to be created', (done)=>{
-        const member_signup = {
+    it('/api/register/ does not allow duplicate member usernames to be created', (done)=>{
+        const member_register = {
             username: "test_user_" + i,
             password: "password_" + i,
             full_name: `Jane ${i} Doe`,
             email: `jane${i}@msu.edu`
         }
-        console.log(member_signup)
+        console.log(member_register)
         request(app)
-        .post("/api/signup")
-        .send(member_signup)
+        .post("/api/register")
+        .send(member_register)
         .expect(307, function(err, res){
             if (err) throw err;
 
             request(app)
-            .post("/api/signup")
-            .send(member_signup)
+            .post("/api/register")
+            .send(member_register)
             .expect(401,/"username must be unique"/,done)
         })
     })
 
     it('/api/login/ allows user to login', (done)=>{
-        const member_signup = {
+        const member_register = {
             username: "test_user_" + i,
             password: "password_" + i,
             full_name: `Jane ${i} Doe`,
             email: `jane${i}@msu.edu`
         }
-        console.log(member_signup)
+        console.log(member_register)
         request(app)
-        .post("/api/signup")
-        .send(member_signup)
+        .post("/api/register")
+        .send(member_register)
         .expect(307, function(err, res){
             if (err) throw err;
 
             request(app)
             .post("/api/login")
-            .send(member_signup)
+            .send(member_register)
             .expect(200,done)
         })
     })
 
     it('/api/login/ allows user to login and logout', (done)=>{
-        const member_signup = {
+        const member_register = {
             username: "test_user_" + i,
             password: "password_" + i,
             full_name: `Jane ${i} Doe`,
             email: `jane${i}@msu.edu`
         }
-        console.log(member_signup)
+        console.log(member_register)
         request(app)
-        .post("/api/signup")
-        .send(member_signup)
+        .post("/api/register")
+        .send(member_register)
         .expect(307, function(err, res){
             if (err) throw err;
 
             request(app)
             .post("/api/login")
-            .send(member_signup)
+            .send(member_register)
             .expect(200, function(err, res){
                 if (err) throw err;
 
