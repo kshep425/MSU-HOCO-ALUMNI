9b53f35acb82dc63549470c6c25b4c4c668c5af0
Keisha <kshep425@gmail.com>
2020-02-16 14:29:55 -0500

    Added an get event time method

--- public/assets/javascript/eventbrite.js
+++ public/assets/javascript/eventbrite.js
@@ -5,23 +5,18 @@ $(document).ready(() => {
         console.log("Submit Event Button clicked")
         let event_info = get_event_info()
         console.log(event_info)
-        //console.log(message_body)
-        // $.ajax({
-        //     url: "api/register",
-        //     method: "POST",
-        //     body: eventbrite_body,
-        //     data: message_body
-        // })
-        // .then((result) => {
-        //     location.href = "/"
-        //     console.log('Status:', response.statusCode);
-        //     console.log('Headers:', JSON.stringify(response.headers));
-        //     console.log('Response:', body);
-
-        // })
-        // .catch((err) => {
-        //     console.log(err)
-        // })
+        $.ajax({
+             url: "api/register",
+             method: "POST",
+             body: event_info,
+        })
+        .then((result) => {
+             location.href = "/"
+             console.log(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 
     })
 
@@ -29,10 +24,18 @@ $(document).ready(() => {
         let event_info = {
             event_name: $("#event_name").val(),
             event_description: $("#event_description").val(),
-            event_start: $("#event_start").val(),
-            event_end: $("#event_end").val(),
+            event_start: get_event_time($("#event_start").val()),
+            event_end: get_event_time($("#event_end").val()),
             event_format: $("#event_format").val()
         };
         return event_info;
     };
+
+    function get_event_time(local_time){
+        return {
+            utc: "",
+            timezone: "",
+            local: local_time
+        }
+    }
 })
