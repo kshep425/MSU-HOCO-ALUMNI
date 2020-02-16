bcffbb2b76e8b607b1bb7da01e276f86a66c00f3
Keisha <kshep425@gmail.com>
2020-02-16 14:07:12 -0500

    Added eventbrite js for submitting form

--- public/assets/javascript/eventbrite.js
+++ public/assets/javascript/eventbrite.js
@@ -1,44 +1,38 @@
-// require("dotenv").config();
-// //var eb_queries = require("../config/eventbrite/eb_queries")
-// //var request = require('request');
-// const eventbrite_url = "https://www.eventbriteapi.com/v3";
-// const eventbrite_token = process.env.EB_TOKEN;
-// const eventbrite_body =
-// {
-//     "Authorization": "Bearer " + eventbrite_token,
-//     "Content-Type": "application/json"
-// };
-// // Eventbrite-API-Waypoint-Token
-// let eventbrite_api_waypoint_token;
-// let organization_id = process.env.EB_ORG_ID;
+$(document).ready(() => {
 
-// request({
-//     method: 'POST',
-//     url: 'https://www.eventbriteapi.com/v3/organizations/12345/events/',
-//     body: eventbrite_body
-// }, function (error, response, body) {
-// });
+    $("#event_submit_btn").on("click", (event) => {
+        event.preventDefault();
+        console.log("Submit Event Button clicked")
+        let event_info = get_event_info()
+        console.log(event_info)
+        //console.log(message_body)
+        // $.ajax({
+        //     url: "api/register",
+        //     method: "POST",
+        //     body: eventbrite_body,
+        //     data: message_body
+        // })
+        // .then((result) => {
+        //     location.href = "/"
+        //     console.log('Status:', response.statusCode);
+        //     console.log('Headers:', JSON.stringify(response.headers));
+        //     console.log('Response:', body);
 
+        // })
+        // .catch((err) => {
+        //     console.log(err)
+        // })
 
-$("#event_submit_btn").on("click", (event)=>{
-    event.preventDefault();
-    console.log("Submit Event Button clicked")
-    //console.log(message_body)
-    // $.ajax({
-    //     url: "api/register",
-    //     method: "POST",
-    //     body: eventbrite_body,
-    //     data: message_body
-    // })
-    // .then((result) => {
-    //     location.href = "/"
-    //     console.log('Status:', response.statusCode);
-    //     console.log('Headers:', JSON.stringify(response.headers));
-    //     console.log('Response:', body);
+    })
 
-    // })
-    // .catch((err) => {
-    //     console.log(err)
-    // })
-
+    function get_event_info(){
+        let event_info = {
+            event_name: $("#event_name").val(),
+            event_description: $("#event_description").val(),
+            event_start: $("#event_start").val(),
+            event_end: $("#event_end").val(),
+            event_format: $("#event_format").val()
+        };
+        return event_info;
+    };
 })
